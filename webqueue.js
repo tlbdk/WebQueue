682276a3898743f57daaade376695bce520bde1b
Troels Liebe Bentsen <tlb@rapanden.dk>
2011-03-30 21:33:57 +0200

    Made simple sample that show how it works

--- webqueue.js
+++ webqueue.js
@@ -4,6 +4,7 @@ server_port = 8080
 
 console.log('pid ' + process.pid);
 
+var waiting = [];
 var clients = {}
 var queues = {
     'test' : ["{ 'status': 1 }", "{ 'status': 2 }"]
@@ -11,38 +12,54 @@ var queues = {
 
 var server = http.createServer(function (req, res) {
     var commands = req.url.split('/');
-    var queue = queues[commands[1]];
-//    console.log(queues);
-//    console.log(queue);
-//    console.log(commands);
-    
-    if(req.method == 'GET' && queue) {
-        console.log("Got GET from " + req.socket.remoteAddress);
-        var queue_name = commands[1];
-        var client_id = commands[2];
-
-        var client = clients[client_id];
-
-        res.writeHead('200', { "Content-Type": "text/javascript" });
-        if(client) {
-        
-        } else {
-            for(var i = 0; i < queue.length; i++) {
-                res.write(queue[i] + "\n");
-            }
-            clients[client_id] = queue.length;
-        }
+    var queue_name = commands[1];
+    var client_id = commands[2];
+    var queue = queues[queue_name];
+   
+    if(!queue) {
+        res.writeHead('404', { "Content-Type": "text/javascript" });
         res.end();
+
+    } else if(req.method == 'GET') {
+        console.log("Got GET from " + req.socket.remoteAddress);
+        res.writeHead('200', { "Content-Type": "text/javascript" });
+
+        var last_i = clients[client_id];
+        if(!last_i) {
+            last_i = 0;
+        }
+
+        if(last_i < queue.length) {
+            res.write("[" + queue.splice(last_i).join(",") + "]\n");
+            res.end();
+            clients[client_id] = queue.length;
+        } else {
+            waiting.push([client_id, res]);
+        }
     
-    } else if (req.method == 'PUT') {
-        console.log("Got PUT from " + req.socket.remoteAddress);
+    } else if (req.method == 'POST') {
+        console.log("Got POST from " + req.socket.remoteAddress);
+        req.addListener('data', function(chunk) {
+            console.log("added data:" + chunk);
+            queue.push(chunk); //TODO add to body and to queue in end 
+        });
+        req.addListener('end', function() {
+            while(client = waiting.shift()) {
+                var client_id = client[0]; 
+                var client_res = client[1]; 
+                var last_i = clients[client_id];
+                client_res.write("[" + queue.splice(last_i).join(",") + "]\n");
+                client_res.end();
+                clients[client_id]++;
+            }
+        });
+        res.writeHead('200', { "Content-Type": "text/javascript" });
+        res.end("{status:'ok'}\n");          
     
     } else {
         res.writeHead('404', { "Content-Type": "text/javascript" });
         res.end();
     }
-   
-//    console.log(clients);
 });
 
 server.listen(server_port, function () {
